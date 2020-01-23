af8c2f0c30f34dc87d4372fb0fbacb116438e9ba
Jessica Abplanalp <57146920+jessicabplanalp@users.noreply.github.com>
2020-01-23 14:42:45 -0800

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -8,22 +8,22 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     id: 'mapbox/streets-v10',
 }).addTo(map);
 
-function onLocationFound(e) {
-    var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method divided by 2. It is divided by 2 because the accuracy value is the sum of the estimated accuracy of the latitude plus the estimated accuracy of the longitude. The unit is meters.
+//function onLocationFound(e) {
+//    var radius = e.accuracy; //this defines a variable radius as the accuracy value returned by the locate method divided by 2. It is divided by 2 because the accuracy value is the sum of the estimated accuracy of the latitude plus the estimated accuracy of the longitude. The unit is meters.
+//
+//    L.marker(e.latlng).addTo(map)  //this adds a marker at the lat and long returned by the locate function.
+//        .bindPopup("You are within " + Math.round(radius * 3.28084) + " feet from this point").openPopup(); //this binds a popup to the marker. The text of the popup is defined here as well. Note that we multiply the radius by 3.28084 to convert the radius from meters to feet and that we use Math.round to round the conversion to the nearest whole number.
 
-    L.marker(e.latlng).addTo(map)  //this adds a marker at the lat and long returned by the locate function.
-        .bindPopup("You are within " + Math.round(radius * 3.28084) + " feet from this point").openPopup(); //this binds a popup to the marker. The text of the popup is defined here as well. Note that we multiply the radius by 3.28084 to convert the radius from meters to feet and that we use Math.round to round the conversion to the nearest whole number.
+//    L.circle(e.latlng, radius).addTo(map); //this adds a circle to the map centered at the lat and long returned by the locate function. Its radius is set to the var radius defined above.
+//}
 
-    L.circle(e.latlng, radius).addTo(map); //this adds a circle to the map centered at the lat and long returned by the locate function. Its radius is set to the var radius defined above.
-}
+//map.on('locationfound', onLocationFound); //this is the event listener
 
-map.on('locationfound', onLocationFound); //this is the event listener
+//function onLocationError(e) {
+//  alert(e.message);
+//}
 
-function onLocationError(e) {
-  alert(e.message);
-}
-
-map.on('locationerror', onLocationError);
+//map.on('locationerror', onLocationError);
 
 //At the bottom of your JavaScript code, call the locate method with the following options:
-map.locate({setView: true, maxZoom: 16});
+//map.locate({setView: true, maxZoom: 16});
